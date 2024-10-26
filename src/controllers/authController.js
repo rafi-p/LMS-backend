2a4e1427e6d8fcdde514fae71fba5e51108225bd
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2024-10-26 22:52:29 +1100

    Sign in api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import TransactionModel from "../models/transactionModel.js";
+import transactionModel from "../models/transactionModel.js";
+import jwt from "jsonwebtoken";
 
 export const SignUpAction = async (req, res) => {
   const midtransUrl = process.env.MIDTRANS_URL;
@@ -56,6 +58,67 @@ export const SignUpAction = async (req, res) => {
       message: "Sign up success",
       data: {
         midtrans_payment_url: resMidtrans.redirect_url,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const SignInAction = async (req, res) => {
+  try {
+    const body = req.body; // email password
+    const existingUser = await userModel
+      .findOne()
+      .where("email")
+      .equals(body.email);
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "Email not found",
+      });
+    }
+    const comparePassword = bcrypt.compareSync(
+      body.password,
+      existingUser.password
+    );
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: "Email / Password incorrect",
+      });
+    }
+
+    const isValidUser = await transactionModel.findOne({
+      user: existingUser._id,
+      status: "success",
+    });
+
+    if (existingUser.role !== "student" && !isValidUser) {
+      return res.status(400).json({
+        message: "User not verified",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        data: {
+          id: existingUser._id.toString(),
+        },
+      },
+      process.env.SECRET_KEY_JWT,
+      { expiresIn: "1 days" }
+    );
+
+    return res.json({
+      message: "User logged in success",
+      data: {
+        name: existingUser.name,
+        email: existingUser.email,
+        token,
+        role: existingUser.role,
       },
     });
   } catch (error) {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { validateRequest } from "../middlewares/validateRequest.js";
-import { signUpSchema } from "../utils/schema.js";
-import { SignUpAction } from "../controllers/authController.js";
+import { signInSchema, signUpSchema } from "../utils/schema.js";
+import { SignInAction, SignUpAction } from "../controllers/authController.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/sign-up", validateRequest(signUpSchema), SignUpAction);
+authRoutes.post("/sign-in", validateRequest(signInSchema), SignInAction);
 
 export default authRoutes;
